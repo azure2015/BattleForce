8088a452c5629cfb61ee5a310c82006f2030cad2
Azure Sky <azuremmii@hotmail.com>
2021-12-14 00:40:10 +0000

    Added death effect to enemy

--- BattleForce/Assets/Scripts/Health.cs
+++ BattleForce/Assets/Scripts/Health.cs
@@ -8,11 +8,33 @@ public class Health : MonoBehaviour
     [SerializeField] ParticleSystem particleSystem;
 
     float delayTime = 0.5f;
+    float angleDirection = 0.5f;
+    bool isDead = false;
 
+    void Start()
+    {
+        float randomNumber = Random.Range(1, 3);
+        if(randomNumber >1)
+        {
+            angleDirection = -0.5f;
+        }
+        Debug.Log(randomNumber);
+    }
+    void Update()
+    {
+        if(isDead)
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 0f, 1f), angleDirection) ;
+            gameObject.transform.localScale += new Vector3(-0.005f, -0.005f, 0f);
+        }
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag=="Bullet")
         {
+            var getCollider = gameObject.GetComponent<CircleCollider2D>();
+            getCollider.enabled = false;
+            isDead = true;
             PlayParticleHit();
             Destroy(gameObject, delayTime);
 
